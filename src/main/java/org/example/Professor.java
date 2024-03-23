b0c51d0a272a8516c5f7ef501931761eece69b25
leofiore <leonardo.matias@gec.inatel.br>
2024-03-23 14:30:53 -0300

    :sparkles:	feat: criando a classe professor

---
+++ src/main/java/org/example/Professor.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Professor {
+
+        private String nomeDoProfessor;
+        private String horarioDeAtendimento;
+        private String periodo;
+        private int sala;
+        private int predio;
+
+    public Professor(String nomeDoProfessor, String horarioDeAtendimento, String periodo, int sala, int predio) {
+            this.nomeDoProfessor = nomeDoProfessor;
+            this.horarioDeAtendimento = horarioDeAtendimento;
+            this.periodo = periodo;
+            this.sala = sala;
+            this.predio = predio;
+        }
+
+    public String getNomeDoProfessor() {
+        return nomeDoProfessor;
+    }
+
+    public String getHorarioDeAtendimento() {
+        return horarioDeAtendimento;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public int getSala() {
+        return sala;
+    }
+
+    public int getPredio() {
+        return predio;
+    }
+      
+}
+
