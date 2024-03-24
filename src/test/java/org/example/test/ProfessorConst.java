8d5a8533e66bdf265a8ae0be286b85ac71432dbb
leofiore <leonardo.matias@gec.inatel.br>
2024-03-24 13:59:52 -0300

    :construction: atualizando o gson

--- src/test/java/org/example/test/ProfessorConst.java
+++ src/test/java/org/example/test/ProfessorConst.java
@@ -4,66 +4,66 @@ public class ProfessorConst {
 
         public static String CRIS =
                 "{ \"nomeDoProfessor\": \"Cris\", \n " +
-                        "\"horarioDeAtendimento\": \"15:30\", \n " +
+                        "\"horarioDeAtendimento\": \"segunda-feira - 15:30\", \n " +
                         "\"periodo\": \"Integral\", \n " +
                         "\"sala\": 2, \n " +
-                        "\"predio\": 1 }";
+                        "\"predio\": [1] }";
                     
         public static String  DANIELA =
                 "{ \"nomeDoProfessor\": \"Daniela\", \n " +
-                        "\"horarioDeAtendimento\": \"17:30\", \n " +
+                        "\"horarioDeAtendimento\": \"terça-feira - 17:30\", \n " +
                         "\"periodo\": \"Integral\", \n " +
                         "\"sala\": 8, \n " +
-                        "\"predio\": 2 }";
+                        "\"predio\": [2] }";
                         
         public static String RENAN =
                 "{ \"nomeDoProfessor\": \"Renan\", \n " +
-                        "\"horarioDeAtendimento\": \"19:30\", \n " +
+                        "\"horarioDeAtendimento\": \"quarta-feira - 19:30\", \n " +
                         "\"periodo\": \"Noturno\", \n " +
                         "\"sala\": 12, \n " +
-                        "\"predio\": 3 }";
+                        "\"predio\": [3] }";
 
         public static String LUIZ =
                 "{ \"nomeDoProfessor\": \"Luiz\", \n " +
-                        "\"horarioDeAtendimento\": \"13:30\", \n " +
+                        "\"horarioDeAtendimento\": \"quinta-feira - 13:30\", \n " +
                         "\"periodo\": \"Integral\", \n " +
                         "\"sala\": 16, \n " +
-                        "\"predio\": 4 }";
+                        "\"predio\": [4] }";
 
         public static String RENZO =
                 "{ \"nomeDoProfessor\": \"Renzo\", \n " +
-                        "\"horarioDeAtendimento\": \"13:30\", \n " +
+                        "\"horarioDeAtendimento\": \"sexta-feira - 13:30\", \n " +
                         "\"periodo\": \"Noturno\", \n " +
                         "\"sala\": 22, \n " +
-                        "\"predio\": 6 }";
+                        "\"predio\": [6] }";
 
         public static String YNOGUTI =
                 "{ \"nomeDoProfessor\": \"Ynoguti\", \n " +
-                        "\"horarioDeAtendimento\": \"10:00\", \n " +
+                        "\"horarioDeAtendimento\": \"segunda-feira - 10:00\", \n " +
                         "\"periodo\": \"Integral\", \n " +
                         "\"sala\": 4, \n " +
-                        "\"predio\": 1 }";
+                        "\"predio\": [1] }";
 
         public static String VITORIA =
                 "{ \"nomeDoProfessor\": \"Vitoria\", \n " +
-                        "\"horarioDeAtendimento\": \"21:30\", \n " +
+                        "\"horarioDeAtendimento\": \"terça-feira - 21:30\", \n " +
                         "\"periodo\": \"Integral\", \n " +
                         "\"sala\": 10, \n " +
-                        "\"predio\": 2 }";
+                        "\"predio\": [2] }";
 
         public static String SONED =
                 "{ \"nomeDoProfessor\": \"Soned\", \n " +
-                        "\"horarioDeAtendimento\": \"19:30\", \n " +
+                        "\"horarioDeAtendimento\": \"quarta-feira - 19:30\", \n " +
                         "\"periodo\": \"Noturno\", \n " +
                         "\"sala\": 14, \n " +
-                        "\"predio\": 3 }";
+                        "\"predio\": [3] }";
 
         public static String INEXISTENTE =
                 "{ \"nomeDoProfessor\": \"INEXISTENTE\", \n " +
                         "\"horarioDeAtendimento\": \"INEXISTENTE\", \n " +
                         "\"periodo\": \"INEXISTENTE\", \n " +
                         "\"sala\": 0, \n " +
-                        "\"predio\": 0 }";
+                        "\"predio\": [0] }";
 
 }
 
